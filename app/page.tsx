a6ecd95d38141782483c4cffe7dd7e7c8c52bfd1
Aleksa <aleksa.mitic5859@gmail.com>
2023-07-14 15:12:22 +0200

    coneccting planet scale and auth

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,19 @@ import Link from 'next/link'
 export default function Home() {
 
 
-  let href = '/';
+  let href = '/journal';
 
   return (
-  <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+  <div className="flex items-center justify-center w-screen h-screen text-white bg-black">
       <div className="w-full max-w-[600px] mx-auto">
-        <h1 className="text-6xl mb-4">The best Journal app, period.</h1>
-        <p className="text-2xl text-white/60 mb-4">
+        <h1 className="mb-4 text-6xl">The best Journal app, period.</h1>
+        <p className="mb-4 text-2xl text-white/60">
           This is the best app for tracking your mood through out your life. All
           you have to do is be honest.
         </p>
         <div>
           <Link href={href}>
-            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+            <button className="px-4 py-2 text-xl bg-blue-600 rounded-lg">
               get started
             </button>
           </Link>
